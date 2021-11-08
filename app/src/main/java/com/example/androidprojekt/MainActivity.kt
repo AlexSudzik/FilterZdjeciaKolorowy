55a3f5c9214316aa2df2d4a7aee1cc82532da107
Alex Sudzik <alex.sudzik@gmail.com>
2021-11-08 15:48:48 +0100

    Prawie Dziala

---
+++ app/src/main/java/com/example/androidprojekt/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.androidprojekt
+
+import android.graphics.Color.red
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ToggleButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val john: ImageView = findViewById(R.id.john)
+        val Bred: ToggleButton = findViewById(R.id.Bred)
+        val Bblue: ToggleButton = findViewById(R.id.Bblue)
+        val Bgreen: ToggleButton = findViewById(R.id.Bgreen)
+
+        Bred.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                john.setColorFilter(null);
+                john.setColorFilter(resources.getColor(R.color.redColor));
+
+                Bblue.setChecked(false);
+                Bgreen.setChecked(false);
+            } else {
+                john.setColorFilter(null);
+                Bblue.setChecked(false);
+                Bgreen.setChecked(false);
+            }
+        }
+
+        Bblue.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                john.setColorFilter(null);
+                john.setColorFilter(resources.getColor(R.color.blueColor));
+
+                Bred.setChecked(false);
+                Bgreen.setChecked(false);
+            } else {
+                john.setColorFilter(null);
+                Bred.setChecked(false);
+                Bgreen.setChecked(false);
+            }
+        }
+
+        Bgreen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                john.setColorFilter(null);
+                john.setColorFilter(resources.getColor(R.color.greenColor));
+
+                Bblue.setChecked(false);
+                Bred.setChecked(false);
+            } else {
+                john.setColorFilter(null);
+                Bblue.setChecked(false);
+                Bred.setChecked(false);
+            }
+        }
+
+
+    }
+}
